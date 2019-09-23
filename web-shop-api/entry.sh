aedac2ce27a1f6676f0f20854bff15dd74068c1a
coa <akism89@gmail.com>
2019-09-23 19:16:25 +0200

    setup

--- web-shop-api/entry.sh
+++ web-shop-api/entry.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 service php7.3-fpm start
 service nginx start
+php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+php -r "if (hash_file('sha384', 'composer-setup.php') === 'a5c698ffe4b8e849a443b120cd5ba38043260d5c4023dbf93e1558871f1f07f58274fc6f4c93bcfd858c6bd0775cd8d1') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
+php composer-setup.php --install-dir=/usr/bin --filename=composer
+chmod +x /usr/bin/composer
+source ~/.bashrc
+cd /var/www/html
+composer install
+php -r "unlink('composer-setup.php');"
 tail -f /dev/null
