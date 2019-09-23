f0a5642b1dfc25b4bf91fa3f42f2e1f76f3b3a60
coa <akism89@gmail.com>
2019-09-23 19:50:27 +0200

    setup

--- web-shop-api/config/cors.php
+++ web-shop-api/config/cors.php
@@ -10,10 +10,10 @@ return [
     | allowedOrigins, allowedHeaders and allowedMethods can be set to array('*')
     | to accept any value.
     |
-    */
+     */
 
     'supportsCredentials' => false,
-    'allowedOrigins' => ['http://localhost:8888','http://0.0.0.0:8888'],
+    'allowedOrigins' => ['http://localhost:8888', 'http://0.0.0.0:8888'],
     'allowedOriginsPatterns' => [],
     'allowedHeaders' => ['*'],
     'allowedMethods' => ['*'],
